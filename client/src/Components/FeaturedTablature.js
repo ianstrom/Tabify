b20295265ba1144fc7cbd5a4545d6c7468f72bcc
ianstrom <iankstrom@gmail.com>
2023-05-24 16:50:24 -0400

    adding check session to edit project

--- client/src/Components/FeaturedTablature.js
+++ client/src/Components/FeaturedTablature.js
@@ -14,7 +14,7 @@ function FeaturedTablature({ user, setProjectToView, featuredTabs }) {
 
     return (
         <div style={{ height: 'auto' }} className='h-full max-w-full mx-auto flex flex-col border rounded-md mb-10 flex-grow'>
-            <h1 onClick={handleClick} className="text-4xl mx-auto md:text-6xl cursor-pointer mb-4 ml-6 mt-2 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:text-indigo-700 hover:scale-105">Your Projects</h1>
+            <h1 onClick={handleClick} className="text-4xl mx-auto md:text-6xl cursor-pointer mb-4 ml-6 mt-2 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:text-indigo-700 hover:scale-105">Featured Projects</h1>
             <div className="m-2 flex flex-wrap justify-center items-center gap-4 md:m-4 max-w-full">
                 {featuredProjectsToDisplay}
             </div>
